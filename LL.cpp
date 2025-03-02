cdf6ed2c46d4ad05e9bd9fcfc799152b4bc0d65c
pawan <pawansinghshiva2030@gmail.com>
2025-03-02 19:24:44 +0530

    ll

--- LL.cpp
+++ LL.cpp
@@ -19,7 +19,7 @@ struct Node{
 Node* convertArr2LL(vector<int> arr){
     Node* head= new Node(arr[0]);
     Node* mover=head;
-    for(int i=0;i<arr.size();i++){
+    for(int i=1;i<arr.size();i++){
         Node* temp= new Node(arr[i]);
         mover->next=temp;
         mover=temp;
